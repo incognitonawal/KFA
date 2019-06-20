898fe39516459675f7b28a22c9df3ea14b2e6070
Sharma <nawal.sharma@cgi.com>
2019-06-20 14:16:39 +0530

    Added first commit

---
+++ KFAServices/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace KFAServices.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ LearningConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LearningConsoleApp
+{
+    public class A
+    {
+        public virtual void m1()
+        {
+            Console.WriteLine("Method of Base CLass");
+            Console.ReadLine();
+        }
+    }
+    public class B:A
+    {
+        public override void m1()
+        {
+            Console.WriteLine("Method of Derived CLass");
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            b.m1();
+        }
+        
+    }
+}
